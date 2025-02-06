d0f2b3932a7bc4811a03477a180d5eaede136e08
Victor P <vlsmarthen@gmail.com>
2025-02-06 18:49:47 -0500

    Fixed/added first upgrade

--- cookies.js
+++ cookies.js
@@ -3,9 +3,10 @@
 var cperclick = 1;
 var cookies = 0;
 var button = document.getElementById("cookieUpgrade0");
-//var upgrade1cost = 10;
+
 var upgrade1level = 1;
-document.getElementById("upgrade1costindicator").innerHTML=upgrade1cost;
+var upgrade1cost = (10 * upgrade1level ** 1.15);
+//document.getElementById("upgrade1costindicator").innerHTML=upgrade1cost;
 console.log("%d",cookies)
 function update(){
     document.getElementById("cookiestext").innerHTML=cookies;
@@ -17,17 +18,19 @@ function addCookie(){
     document.getElementById("cookiestext").innerHTML=cookies;
 }
 function upgrade1(){  
-    cost = Math.round(10 * upgrade1level ** 1.15);  
-    if (cookies >= cost){
-        
+    var upgrade1maxlevel = 20;
+    if (cookies >= upgrade1cost && upgrade1level < upgrade1maxlevel){
+        cookies -= upgrade1cost;    
+        cperclick += 1;    
         upgrade1level += 1;
-        cost = Math.round(10 * upgrade1level ** 1.15);  
-        console.log("the cost of upg 1 is now %d",cost)
-        document.getElementById("upgrade1costtext").innerHTML=cost;
-        
-        cperclick += 1;
-        cookies -= cost; 
+        upgrade1cost = Math.round(10 * upgrade1level ** 1.15);  
+        console.log("the cost of upg 1 is now %d",upgrade1cost);
         update();
+        document.getElementById("upgrade1costindicator").innerHTML=upgrade1cost;
+        document.getElementById("upg1level").innerHTML=upgrade1level;
+        
+        
+       
         
     }
    

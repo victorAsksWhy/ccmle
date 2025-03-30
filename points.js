a41e7e5927dcb8119b6ab0b70ca89884efae5de6
Victor P <vlsmarthen@gmail.com>
2025-03-30 12:53:45 -0400

    Reablencde upg12 and 13
    U12 max level 5 -> 7
    U12 starting cost 50->75
    U13 buff 30 -> 25

--- points.js
+++ points.js
@@ -1,4 +1,4 @@
-//const { sleep } = require("openai/core.mjs");
+// dont elete or else
 var baseppc = 1;
 var pointsPerClick = 1;
 var points = 0;
@@ -8,6 +8,12 @@ var upgrade1cost = (10 * upgrade1level ** 1.15);
 var upgrade1bonus = 0;
 var cpcBonusUpgrade2 = 0;
 var upgrade2level = 0;
+var upgrade3level = 0;
+var boughtUpgrade3 = 0;
+function setpoints(n){ //🤫
+    points=n;
+    update();
+}
 //document.getElementById("upgrade1costindicator").innerHTML=upgrade1cost;
 console.log("%d",points)
 function update(){
@@ -19,7 +25,7 @@ function update(){
     console.log("updated to %d", points)
 }
 function addPoint(){
-    var boostedBase = baseppc*(2**upgrade2level);
+    var boostedBase = baseppc+25*boughtUpgrade3*(2**upgrade2level);
     var pointsPerClick = boostedBase+upgrade1bonus;
     console.log("ppc should be %d",pointsPerClick);
     console.log("from %d base ppc", baseppc);
@@ -47,18 +53,33 @@ function upgrade1(){
     }   
 }
 //upgrade 2 things
-var upgrade2cost = 50;
-var upgrade2maxlevel = 5;
+var upgrade2cost = 75;
+var upgrade2maxlevel = 7;
 
 function upgrade2(){  
     if (points >= upgrade2cost && upgrade2level < upgrade2maxlevel){
         points -= upgrade2cost;    
         upgrade2level += 1;
-        upgrade2cost = 100*2**upgrade2level;
+        upgrade2cost = 75*2**upgrade2level;
         console.log("the cost of upg 2 is now %d",upgrade2cost);
         console.log("the level is %d", upgrade2level)
         update();
         document.getElementById("upgrade2costindicator").innerHTML=upgrade2cost;
         document.getElementById("upg2level").innerHTML=upgrade2level;     
     }   
+}
+
+//upgrade 3 things
+
+var upgrade3cost = 750;
+var upgrade3maxlevel = 1;
+function upgrade3(){
+    if (points >= upgrade3cost && upgrade3level < upgrade3maxlevel){
+        boughtUpgrade3 = 1;
+        points -= upgrade3cost;
+        upgrade3level += 1;
+        console.log("bought upgrade 3");
+        update();
+        document.getElementById("upg3level").innerHTML=upgrade3level;
+    }
 }
